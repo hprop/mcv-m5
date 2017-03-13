3f1053cd1c1e487184ab8ccb8e4b0f8f4d677cda
Hugo Prol <hugo.prol.pereira@gmail.com>
2017-03-13 23:00:52 +0100

    Add optional dropout to densenet and fix some function calls

--- code/models/densenet.py
+++ code/models/densenet.py
@@ -21,16 +21,19 @@ import math
 
 
 
-def transition(x, n_filter, weight_decay=1E-4):
+def transition(x, n_filter, dropout=None, weight_decay=1E-4):
     '''
     Transition layer on the Densenet
 
     :param x: Input features (4D tensor -> numpy array)
     :param n_filter: number of feature maps on the 2DConv
+    :param dropout: None or float. When it is not None, apply dropout layers
+        after each 2DConv layer with probability to drop equal to this value.
     :param weight_decay: weight decay on the regularizers for BN and 2DConv
-    :return: Output features (4D tensor -> numpy array)
-    '''
 
+    :return: Output features (4D tensor -> numpy array)
+
+    '''
     x = BatchNormalization(mode=0,
                            axis=1,
                            gamma_regularizer=l2(weight_decay),
@@ -41,12 +44,17 @@ def transition(x, n_filter, weight_decay=1E-4):
                       border_mode='same',
                       bias=False,
                       W_regularizer=l2(weight_decay))(x)
+
+    if dropout is not None:
+        x = Dropout(dropout)(x)
+
     x = AveragePooling2D((2, 2), strides=(2, 2))(x)
 
     return x
 
 
-def denseblock(x, n_layers, n_filter, n_bottleneck=None, weight_decay=1E-4):
+def denseblock(x, n_layers, n_filter, n_bottleneck=None, dropout=None,
+               weight_decay=1E-4):
     '''
     Dense block on the Densenet
 
@@ -54,11 +62,14 @@ def denseblock(x, n_layers, n_filter, n_bottleneck=None, weight_decay=1E-4):
     :param n_layers: Number of layers on the block
     :param n_filter: number of 3x3 feature maps on the 2DConv layers
     :param n_bottleneck: number of 1x1 feature maps on the 2DConv bottleneck
-    layers. If None, no bottleneck algorithm will be applied.
+        layers. If None, no bottleneck algorithm will be applied.
+    :param dropout: None or float. When it is not None, apply dropout layers
+        after each 2DConv layer with probability to drop equal to this value.
     :param weight_decay: weight decay on the regularizers for BN and 2DConv
-    :return: Output features (4D tensor -> numpy array)
-    '''
 
+    :return: Output features (4D tensor -> numpy array)
+
+    '''
     list_feat = [x]
 
     if K.image_dim_ordering() == 'th':
@@ -78,6 +89,9 @@ def denseblock(x, n_layers, n_filter, n_bottleneck=None, weight_decay=1E-4):
                               border_mode='same',
                               bias=False,
                               W_regularizer=l2(weight_decay))(x)
+            if dropout is not None:
+                x = Dropout(dropout)(x)
+
         x = BatchNormalization(mode=0,
                                axis=1,
                                gamma_regularizer=l2(weight_decay),
@@ -88,6 +102,8 @@ def denseblock(x, n_layers, n_filter, n_bottleneck=None, weight_decay=1E-4):
                           border_mode='same',
                           bias=False,
                           W_regularizer=l2(weight_decay))(x)
+        if dropout is not None:
+            x = Dropout(dropout)(x)
 
         list_feat.append(x)
         x = merge(list_feat, mode='concat', concat_axis=concat_axis)
@@ -102,9 +118,10 @@ def classification_block(x, n_classes, weight_decay=1E-4):
     :param x: Input features (4D tensor -> numpy array)
     :param n_classes: Number of classes on the dataset, it is used by softmax
     :param weight_decay: weight decay on the regularizers for BN and 2DConv
-    :return: Output features (4D tensor -> numpy array)
-    '''
 
+    :return: Output features (4D tensor -> numpy array)
+
+    '''
     x = BatchNormalization(mode=0,
                            axis=1,
                            gamma_regularizer=l2(weight_decay),
@@ -121,21 +138,25 @@ def classification_block(x, n_classes, weight_decay=1E-4):
 def build_densenet(img_shape=(3, 224, 224), n_classes=1000,
                    layers_in_dense_block=[6, 12, 24, 16], initial_filters=16,
                    growth_rate=32, n_bottleneck=None, compression=1.,
-                   weight_decay=0.):
+                   dropout=None, weight_decay=0.):
     '''
     Returns a Densenet model
 
     :param img_shape: Shape of the input images for the network. Tuple of 3
-    containing channels, roes and columns.
+        containing channels, roes and columns.
     :param n_classes: Number of classes on the dataset.
     :param layers_in_dense_block: Number of layers on each dense block. List.
     :param initial_filters: Number of filters applied on the first 2DConv layer.
     :param growth_rate: Growth rate of the network.
     :param n_bottleneck: number of 1x1 feature maps on the 2DConv bottleneck
-    layers. If None, no bottleneck algorithm will be applied.
+        layers. If None, no bottleneck algorithm will be applied.
     :param compression: Compression rate for the transition layers.
+    :param dropout: None or float. When it is not None, apply dropout layers
+        after each 2DConv layer with probability to drop equal to this value.
     :param weight_decay: weight decay on the regularizers for BN and 2DConv.
+
     :return: the Densenet model
+
     '''
 
     model_input = Input(shape=img_shape)
@@ -149,13 +170,15 @@ def build_densenet(img_shape=(3, 224, 224), n_classes=1000,
                       bias=False)(model_input)
 
     for block in layers_in_dense_block[:-1]:
-        x = denseblock(x, block, growth_rate,
-                       weight_decay=weight_decay)
+        x = denseblock(x, block, growth_rate, n_bottleneck=n_bottleneck,
+                       dropout=dropout, weight_decay=weight_decay)
         n_filters += math.floor(compression*growth_rate*block)
-        x = transition(x, n_filters, weight_decay=weight_decay)
+        x = transition(x, n_filters, dropout=dropout,
+                       weight_decay=weight_decay)
 
-    x = denseblock(x, layers_in_dense_block[-1], n_filters,
-                   n_bottleneck=n_bottleneck, weight_decay=weight_decay)
+    x = denseblock(x, layers_in_dense_block[-1], growth_rate,
+                   n_bottleneck=n_bottleneck, dropout=dropout,
+                   weight_decay=weight_decay)
 
     x = classification_block(x, n_classes, weight_decay=weight_decay)
 
