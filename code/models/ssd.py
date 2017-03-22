1635e5cc148d3661a15abe26474bdc016c4d69e7
Hugo Prol <hugo.prol.pereira@gmail.com>
2017-03-23 00:15:43 +0100

    WIP: decoupling base network from sdd prediction layers

--- code/models/ssd.py
+++ code/models/ssd.py
@@ -1,8 +1,11 @@
-from keras.layers import (Input, Convolution2D, MaxPooling2D,
+from keras.layers import (Input, Flatten, Convolution2D, MaxPooling2D,
                           AtrousConvolution2D, GlobalAveragePooling2D)
+
+from layers.ssd_layers import Normalize, PriorBox
 
 
 # Sources:
+# - TODO: add paper reference
 # - https://github.com/rykov8/ssd_keras
 
 
@@ -21,7 +24,9 @@ def vgg16_base_network(input_shape=None):
                                    activation='relu',
                                    border_mode='same',
                                    name='conv1_2')(net['conv1_1'])
-    net['pool1'] = MaxPooling2D((2, 2), strides=(2, 2), border_mode='same',
+    net['pool1'] = MaxPooling2D((2, 2),
+                                strides=(2, 2),
+                                border_mode='same',
                                 name='pool1')(net['conv1_2'])
 
     # Block 2
@@ -33,7 +38,9 @@ def vgg16_base_network(input_shape=None):
                                    activation='relu',
                                    border_mode='same',
                                    name='conv2_2')(net['conv2_1'])
-    net['pool2'] = MaxPooling2D((2, 2), strides=(2, 2), border_mode='same',
+    net['pool2'] = MaxPooling2D((2, 2),
+                                strides=(2, 2),
+                                border_mode='same',
                                 name='pool2')(net['conv2_2'])
 
     # Block 3
@@ -49,7 +56,9 @@ def vgg16_base_network(input_shape=None):
                                    activation='relu',
                                    border_mode='same',
                                    name='conv3_3')(net['conv3_2'])
-    net['pool3'] = MaxPooling2D((2, 2), strides=(2, 2), border_mode='same',
+    net['pool3'] = MaxPooling2D((2, 2),
+                                strides=(2, 2),
+                                border_mode='same',
                                 name='pool3')(net['conv3_3'])
 
     # Block 4
@@ -65,7 +74,9 @@ def vgg16_base_network(input_shape=None):
                                    activation='relu',
                                    border_mode='same',
                                    name='conv4_3')(net['conv4_2'])
-    net['pool4'] = MaxPooling2D((2, 2), strides=(2, 2), border_mode='same',
+    net['pool4'] = MaxPooling2D((2, 2),
+                                strides=(2, 2),
+                                border_mode='same',
                                 name='pool4')(net['conv4_3'])
 
     # Block 5
@@ -81,7 +92,9 @@ def vgg16_base_network(input_shape=None):
                                    activation='relu',
                                    border_mode='same',
                                    name='conv5_3')(net['conv5_2'])
-    net['pool5'] = MaxPooling2D((3, 3), strides=(1, 1), border_mode='same',
+    net['pool5'] = MaxPooling2D((3, 3),
+                                strides=(1, 1),
+                                border_mode='same',
                                 name='pool5')(net['conv5_3'])
 
     # Block 6
@@ -145,12 +158,85 @@ def vgg16_base_network(input_shape=None):
     ## image size is > 300x300.
     net['pool11'] = GlobalAveragePooling2D(name='pool11')(net['conv11_2'])
 
+    # Add extra layer on top of conv4_3 to normalize its output according to
+    # the paper
+    net['conv4_3_norm'] = Normalize(20, name='conv4_3_norm')(net['conv4_3'])
+
     return net
 
 
-def build_ssd(base_network, priors):
-    """priors: dict with keys being the names of layers used as feature extractors,
+def create_prior(layer_name, n_priors, min_size, max_size, aspect_ratios,
+                 variances):
+    return dict(layer_name=layer_name, n_priors=n_priors, min_size=min_size,
+                max_size=max_size, aspect_ratios=aspect_ratios,
+                variances=variances)
+
+
+def build_ssd300(input_shape, n_classes):
+
+    vgg16 = vgg16_base_network(input_shape)
+
+    variances = [.1, .1, .2, .2]
+    priors = [create_prior('conv4_3_norm', 3, 30., None, [2], variances),
+              create_prior('conv7', 6, 60., 114., [2, 3], variances),
+              create_prior('conv8_2', 6, 114., 168., [2, 3], variances),
+              create_prior('conv9_2', 6, 168., 222., [2, 3], variances),
+              create_prior('conv10_2', 6, 168., 222., [2, 3], variances),
+              create_prior('conv11_2', 6, 276., 330., [2, 3], variances)]
+
+    ssd300 = build_ssd(input_shape, n_classes, vgg16, priors)
+
+    return ssd300
+
+
+
+
+def build_ssd(input_shape, n_classes, base_network, priors):
+    """
+    input_shape: (h, w)
+
+
+    priors: dict with keys being the names of layers used as feature extractors,
     and values being the priors (bboxes info) corresponding to each layer.
 
     """
-    pass
+
+    net = dict(base_network)
+
+
+    ## For each layer taking as a prediction input:
+
+    base_layer = 'conv4_3_norm'
+    num_priors = 3
+    prior_min_size = 30.
+    prior_max_size = None
+    prior_aspect_ratios = [2]
+    prior_variances = [0.1, 0.1, 0.2, 0.2]
+
+    # Bounding box locations
+    loc_layer = base_layer + '_loc'
+    loc_layer_flat = base_layer + '_loc_flat'
+
+    net[loc_layer] = Convolution2D(num_priors * 4, 3, 3,
+                                   border_mode='same',
+                                   name=loc_layer)(net[base_layer])
+    net[loc_layer_flat] = Flatten(name=loc_layer_flat)(net[loc_layer])
+
+    # Class confidences for each bounding box
+    conf_layer = base_layer + '_conf'
+    conf_layer_flat = base_layer + '_conf_flat'
+
+    net[conf_layer] = Convolution2D(num_priors * n_classes, 3, 3,
+                                    border_mode='same',
+                                    name=conf_layer)(net[base_layer])
+    net[conf_layer_flat] = Flatten(name=conf_layer_flat)(net[conf_layer])
+
+    # Bounding box priors (one for each layer)
+    priors_layer = base_layer + '_priors'
+
+    net[priors_layer] = PriorBox([input_shape[1], input_shape[0]],
+                                 prior_min_size,
+                                 max_size=prior_max_size,
+                                 aspect_ratios=prior_aspect_ratios,
+                                 variances=prior_variances,
+                                 name=priors_layer)(net[base_layer])
