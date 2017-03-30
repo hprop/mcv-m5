7974ff128fdb986b4ba18bee0b71baf9d20dc7f5
MrLeylo <i.masmend@gmail.com>
2017-03-31 00:15:35 +0200

    Script to evaluate datasets added

---
+++ code/eval_dataset.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image
+import numpy as np
+
+
+dataset_path = '../../../datasets_local/Datasets/segmentation/camvid'
+dataset_name = 'Camvid'
+subsets = ['train', 'valid', 'test']
+max_class = 30
+count_res = {}
+
+for sst in subsets:
+    by_classes = np.zeros(max_class)
+    full_path = dataset_path + '/' + sst + '/masks'
+    for img_fname in os.listdir(full_path):
+        full_file = full_path + '/' + img_fname
+        img = Image.open(full_file)
+        im_mat = np.asarray(img)
+        by_classes += np.array([np.count_nonzero(im_mat == i)
+                                for i in range(max_class)])
+    count_res[sst] = by_classes
+
+print('Camvid results:')
+print(count_res)
